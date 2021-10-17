16b70f73a5738884942fc75260ae57017770b166
Oleh Misarosh <olehmisar@gmail.com>
2021-10-17 06:43:44 +0300

    Add rooms

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,11 @@
   import Game from "./Game.svelte";
   import { playerId, username } from "./user-store";
   import UsernameForm from "./UsernameForm.svelte";
+  import { Router, Route, links, navigate } from "svelte-routing";
+  import * as typesaurus from "typesaurus";
+  import { db } from "./db";
+
+  let newGameId = "";
 </script>
 
 {$playerId} - {$username}
@@ -9,5 +14,25 @@
 {#if !$username}
   <UsernameForm />
 {:else}
-  <Game gameId="123" />
+  <div use:links>
+    <Router>
+      <Route path="/play/:gameId" let:params>
+        <Game gameId={params.gameId || ""} />
+      </Route>
+      <Route>
+        <form
+          on:submit|preventDefault={async () => {
+            newGameId = newGameId.trim();
+            if ((await typesaurus.get(db.games, newGameId))?.data) {
+              alert("Room exists");
+            }
+            navigate(`/play/${newGameId}`);
+          }}
+        >
+          <input bind:value={newGameId} placeholder="Room Name" />
+          <button>New game</button>
+        </form>
+      </Route>
+    </Router>
+  </div>
 {/if}
